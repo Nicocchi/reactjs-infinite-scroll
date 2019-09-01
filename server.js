694b77596ebd40c30f25e887f674992d16c98e90
Nicocchi <nicocchilover@gmail.com>
2019-09-01 13:00:48 -0400

    server config and server vars initiated

---
+++ server.js
@@ -0,0 +1,5 @@
+global.fetch = require('node-fetch');
+const config = require('universal-config');
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
+const express = require('express');
